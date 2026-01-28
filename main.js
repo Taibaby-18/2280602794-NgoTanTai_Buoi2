7cb5aca2ca5e4d8939017c3e7c9a51319df23e44
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2026-01-28 10:18:37 +0700

    CRUD post

--- main.js
+++ main.js
@@ -1,28 +1,71 @@
-function LoadData() {
-    fetch("https://api.escuelajs.co/api/v1/products")
-        .then(
-            function (res) {
-                return res.json();
+async function LoadData() {
+    let res = await fetch("http://localhost:3000/posts")
+    let posts = await res.json();
+    let body = document.getElementById("body_table");
+    body.innerHTML = '';
+    for (const post of posts) {
+        body.innerHTML += `<tr>
+            <td>${post.id}</td>
+            <td>${post.title}</td>
+            <td>${post.views}</td>
+           <td><input type="submit" value="Delete" onclick="Delete( ${post.id})"/></td>
+        </tr>`
+    }
+
+}
+async function Save() {
+    let id = document.getElementById("id_txt").value;
+    let title = document.getElementById("title_txt").value;
+    let views = document.getElementById("view_txt").value;
+    let getItem = await fetch('http://localhost:3000/posts/' + id)
+    if (getItem.ok) {
+        let res = await fetch('http://localhost:3000/posts/'+id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title,
+                views: views
+            })
+        });
+        if (res.ok) {
+            console.log("Thanh cong");
+        }
+    } else {
+        try {
+            let res = await fetch('http://localhost:3000/posts', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    id: id,
+                    title: title,
+                    views: views
+                })
+            });
+            if (res.ok) {
+                console.log("Thanh cong");
             }
-        ).then(
-            function (products) {
-                let body = document.getElementById("body_table");
-                body.innerHTML = '';
-                for (const product of products) {
-                    body.innerHTML +=
-                        `<tr>
-                        <td>${product.id}</td>
-                        <td>${product.title}</td>
-                        <td>${product.slug}</td>
-                        <td>${product.price}</td>
-                        <td>${product.description}</td>
-                        <td>${product.category.name}</td>
-                        <td>
-                         <img style="width:60px" src="${product.images[0]}"/>
-                        </td>
-                   </tr>`
-                }
-            }
-        )
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    LoadData();
+    return false;
+
+
+
+}
+async function Delete(id) {
+    let res = await fetch("http://localhost:3000/posts/" + id, {
+        method: 'delete'
+    })
+    if (res.ok) {
+        console.log("Thanh cong");
+    }
+    LoadData();
+    return false;
 }
 LoadData();
